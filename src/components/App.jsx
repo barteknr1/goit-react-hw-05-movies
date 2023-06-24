3f2a0696dc18920de877f7a5bcc54b581516df61
Bartek <Barteknr1@gmail.com>
2023-06-24 16:53:01 +0200

    Added Components and routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home/Home'
+import Movies from './Movies/Movies'
+import MovieDetails from './Movies/MovieDetails/MovieDetails'
+import Cast from './Cast/Cast'
+import Reviews from './Reviews/Reviews'
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route>
           <Route path="/" element={<Home />} />
-        </Route>
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies:movieId" element={<MovieDetails />} />
+          <Route path="/movies:movieId/cast" element={<Cast />} />
+          <Route path="/movies:movieId/reviews" element={<Reviews />} />
       </Routes>
     </>
   )
